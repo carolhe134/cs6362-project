c334a9f93c2425132586507c401bd19611440f8f
Carol He <carolhe.che.17@gmail.com>
2024-12-12 04:33:54 -0600

    update scripts

--- selfies_vae/initial_exps/train_200k.py
+++ selfies_vae/initial_exps/train_200k.py
@@ -1,3 +1,16 @@
+import importlib.util
+import sys
+import os
+
+# Path to selfiesvae
+selfiesvae_path = os.path.abspath(os.path.join(os.getcwd(), "../../selfiesvae"))
+module_name = "selfiesvae"
+
+# Load the module dynamically
+spec = importlib.util.spec_from_file_location(module_name, os.path.join(selfiesvae_path, "__init__.py"))
+selfiesvae = importlib.util.module_from_spec(spec)
+sys.modules[module_name] = selfiesvae
+spec.loader.exec_module(selfiesvae)
 from selfiesvae.train import train
 
 def main():
